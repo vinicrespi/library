4ed4cb70ab459e05621ad0a5f36900c2b2290141
vinicrespi <vinicrespi@gmail.com>
2021-03-22 12:04:51 -0300

    Hide secret key

--- library/settings.py
+++ library/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "q%1sbjumuxj10ekozj0rj-1zq=d=@j-lgd37lj#h=$#o$=ubdn"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
