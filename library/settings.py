b02c725566ad7cb9abcd6c4429b0e1e751b5e6cf
vinicrespi <vinicrespi@gmail.com>
2021-03-22 16:55:15 -0300

    update secret key to use env

--- library/settings.py
+++ library/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+SECRETE_KEY = 'secret_key_here'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
